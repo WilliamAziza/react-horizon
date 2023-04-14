83974d19a172a4ddba7bee63b695f0e46adae7bc
William Aziza <Williamaziza37@gmail.com>
2023-04-14 21:36:10 +0000

    horizon

---
+++ src/AlbumCat.jsx
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+
+class AlbumCat extends Component {
+    render() {
+        return (
+            <div>
+                <div class="container Pizza-types">
+    <div class="row">
+      <h3 class="text-center">ALBUM CATALOGUE</h3>
+      <div class="col-md-3">
+        <div class="thumbnail">
+      <img src="images/highest.jpg" alt="Highest"/>
+      <div class="caption">
+        <h3>HIGHEST ALBUM</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        
+      </div>
+    </div>
+      </div> 
+      <div class="col-md-3">
+        <div class="thumbnail">
+      <img src="images/no pressure.jpg" alt="..." width="700PX"/>
+      <div class="caption">
+        <h3>NO PRESSURE ALBUM</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        
+      </div>
+    </div>
+      </div> 
+      <div class="col-md-3">
+        <div class="thumbnail">
+      <img src="images/jamz.jpg" alt="..."/>
+      <div class="caption">
+        <h3>JAMZ ALBUM</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        
+      </div>
+    </div>
+      </div> 
+      <div class="col-md-3">
+        <div class="thumbnail">
+      <img src="images/blacklove.jpg" alt="..."/>
+      <div class="caption">
+        <h3>BLACKLOVE ALBUM</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </div>
+      </div>
+    </div>
+    
+  </div>
+            </div>
+        );
+    }
+}
+
+export default AlbumCat;
---
+++ src/footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+             <div class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <h1>SPOTIFY MUSIC</h1>
+          <p>@Sarkcess Music</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="col-md-4">
+           <h1>APPLE MUSIC</h1>
+          <p>@SarKodie</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="col-md-4">
+           <h1>BOOMPLAY MUSIC</h1>
+          <p>Michael Owusu</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+        </div>
+    );
+}
+
+export default Footer;
